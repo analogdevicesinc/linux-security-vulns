37bbaf7a6f561c970c416ac6494a8360300945b4
Jorge Marques <jorge.marques@analog.com>
2026-04-20 22:05:49 +0200

    general: many fixes + test script
    Add test script to simplify build and check step, then fix many small
    bugs.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

---
+++ test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Test build + check, with cache for re-run
+
+# -- build --
+
+source ./build-cve-db.sh
+
+mkdir -p build ; cd $_
+
+build-cve-db
+
+cd ..
+export CVEKERNELTREE=
+
+# -- check --
+
+mkdir -p check ; cd $_
+
+[ -d "./vulns" ] && {
+	(cd vulns ; git pull origin master --depth 1)
+} || {
+	git clone --depth 1 --no-checkout --filter=blob:none \
+	    https://git.kernel.org/pub/scm/linux/security/vulns.git \
+	    vulns
+	(cd vulns ; git sparse-checkout set cve/published ; git checkout)
+}
+
+(cd ../build ; cp --parents vulns/tools/target/release/strak ../check )
+(cd ../build ; cp --parents vulns/tools/verhaal/verhaal.db ../check )
+
+get_file () {
+	cp "../$1" .
+}
+
+get_file get-cve-list.sh
+get_file filter-cve-list.sh
+
+chmod +x vulns/tools/target/release/strak
+
+cve_all=$(mktemp)
+source ./get-cve-list.sh
+source ./filter-cve-list.sh
+
+get-cve-list "v6.19.3" "$cve_all"
+
+cat "$cve_all"
+
+filter-cve-list "$cve_all" "../compile_commands.json"
